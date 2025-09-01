a497e08d6a0bab89e883c2d2aebaa7352ae78e84
KristhDev <kristhdev@gmail.com>
2025-08-31 23:43:11 -0600

    feat(controllers): serve generated cv as pdf
    Updates the CV generation endpoint to directly stream the generated PDF content to the client. This allows users to download the CV immediately upon request, rather than just receiving a confirmation message.

--- src/presentation/modules/portfolio/controllers/generate-cv.controller.ts
+++ src/presentation/modules/portfolio/controllers/generate-cv.controller.ts
@@ -11,9 +11,15 @@ export class GenerateCVController extends BaseController {
         super();
     }
 
+    private sendPdfCV(pdfCVBuffer: Uint8Array<ArrayBufferLike>, res: Response): void {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename="Kristhian_Ferrufino_CV.pdf"');
+        res.send(pdfCVBuffer);
+    }
+
     public handle(req: Request, res: Response): void {
         this.generateCVUseCase.execute()
-            .then(() => this.jsonResponse.success(res, { message: 'CV generated successfully' }))
+            .then((pdfCVBuffer) => this.sendPdfCV(pdfCVBuffer, res))
             .catch((error) => this.jsonResponse.error(res, error));
     }
 }
