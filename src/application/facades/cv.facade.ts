6f7817aacf5a1990cda1624314da67e3e175db42
KristhDev <kristhdev@gmail.com>
2025-08-31 23:42:08 -0600

    feat(facades): integrate pdf generation adapter
    Adds the capability to generate a CV in PDF format by integrating a dedicated PDF adapter. The `generateCV` method now returns the binary data of the generated PDF, moving beyond just logging the CV data.

--- src/application/facades/cv.facade.ts
+++ src/application/facades/cv.facade.ts
@@ -1,16 +1,29 @@
 /* Contracts */
+import { PDFAdapterContract } from '@domain/contracts/adapters';
 import { CVFacadeContract } from '@domain/contracts/facades';
 import { PortfolioServiceContract } from '@domain/contracts/services';
 
 export class CVFacade implements CVFacadeContract {
     public constructor (
-        private readonly portfolioService: PortfolioServiceContract
+        private readonly portfolioService: PortfolioServiceContract,
+        private readonly pdfAdapter: PDFAdapterContract
     ) {}
 
-    public async generateCV(): Promise<void> {
+    public async generateCV(): Promise<Uint8Array<ArrayBufferLike>> {
         try {
-            const cvData = await this.portfolioService.getCVData();
-            console.log(JSON.stringify(cvData, null, 2));
+            const cvDataDto = await this.portfolioService.getCVData();
+            // const cvTemplate = this.cvService.generateCVTemplate(cvDataDto);
+
+            const dataString = JSON.stringify({
+                authorImage: cvDataDto.authorImage,
+                summary: cvDataDto.summary,
+                skills: cvDataDto.skills,
+                workingExperiences: cvDataDto.workingExperiences
+            }, null, 2);
+
+            const cvPdf = await this.pdfAdapter.generate(dataString);
+
+            return cvPdf;
         } 
         catch (error) {
             throw error;
