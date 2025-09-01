df9c0a5176ec911a6d464ead56d4fac79182de84
KristhDev <kristhdev@gmail.com>
2025-09-01 13:20:51 -0600

    docs(usecases): add jsdoc to generate cv method
    Adds JSDoc comments to the `execute` method within the CV generation use case. This improves code clarity by documenting the method's purpose and return type, enhancing developer understanding and maintainability.

--- src/application/usecases/cv/generate-cv-usecase.ts
+++ src/application/usecases/cv/generate-cv-usecase.ts
@@ -7,6 +7,11 @@ export class GenerateCVUseCase implements GenerateCVUseCaseContract {
         private readonly cvFacade: CVFacadeContract
     ) {}
 
+    /**
+     * Generates a CV based on the provided CV data.
+     *
+     * @return {Promise<Uint8Array<ArrayBufferLike>>} The generated CV as a PDF.
+     */
     public async execute (): Promise<Uint8Array<ArrayBufferLike>> {
         try {
             return await this.cvFacade.generateCV();
