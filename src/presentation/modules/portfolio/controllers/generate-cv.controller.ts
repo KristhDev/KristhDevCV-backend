d78a1430a14d5edc04f5d2dc8eea6e63fc2de089
KristhDev <kristhdev@gmail.com>
2025-09-01 13:21:09 -0600

    refactor(controllers): set ok status and reorder send cv params
    Explicitly sets the HTTP status to 200 OK when successfully sending the PDF CV,
    improving the API contract and client understanding of the response.
    
    Reorders parameters in the `sendPdfCV` method to enhance consistency and
    readability within the codebase.
    
    Adds comprehensive JSDoc comments to `sendPdfCV` and `handle` methods,
    improving code documentation and maintainability.

--- src/presentation/modules/portfolio/controllers/generate-cv.controller.ts
+++ src/presentation/modules/portfolio/controllers/generate-cv.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 
+/* Controllers */
 import { BaseController } from '@server/controller';
 
+/* Contracts */
 import { GenerateCVUseCaseContract } from '@domain/contracts/usecases/cv';
 
 export class GenerateCVController extends BaseController {
@@ -11,15 +13,29 @@ export class GenerateCVController extends BaseController {
         super();
     }
 
-    private sendPdfCV(pdfCVBuffer: Uint8Array<ArrayBufferLike>, res: Response): void {
+    /**
+     * Sends the PDF CV to the client.
+     *
+     * @param {Response} res - The response object.
+     * @param {Uint8Array<ArrayBufferLike>} pdfCVBuffer - The PDF CV buffer.
+     * @return {void} This function does not return a value.
+     */
+    private sendPdfCV(res: Response, pdfCVBuffer: Uint8Array<ArrayBufferLike>): void {
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename="Kristhian_Ferrufino_CV.pdf"');
-        res.send(pdfCVBuffer);
+        res.status(this.httpStatus.OK).send(pdfCVBuffer);
     }
 
+    /**
+     * Handles the request to generate the CV.
+     *
+     * @param {Request} req - The request object.
+     * @param {Response} res - The response object.
+     * @return {void} This function does not return a value.
+     */
     public handle(req: Request, res: Response): void {
         this.generateCVUseCase.execute()
-            .then((pdfCVBuffer) => this.sendPdfCV(pdfCVBuffer, res))
+            .then((pdfCVBuffer) => this.sendPdfCV(res, pdfCVBuffer))
             .catch((error) => this.jsonResponse.error(res, error));
     }
 }
