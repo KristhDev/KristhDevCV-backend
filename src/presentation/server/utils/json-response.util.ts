f66849a3a64c9344bbf7f3fdcb53016493980b0c
KristhDev <kristhdev@gmail.com>
2025-08-31 21:49:30 -0600

    feat(utils): add json response utility
    Introduces a utility class to standardize JSON response handling across the presentation layer. Provides consistent methods for sending success and various HTTP error responses, leveraging HttpClientError for uniform error formatting.

---
+++ src/presentation/server/utils/json-response.util.ts
@@ -0,0 +1,41 @@
+import { Response } from 'express';
+
+import { httpStatus } from '@application/constants';
+
+import { HttpClientError } from '@domain/errors';
+
+export interface JsonResponseData {
+    message?: string;
+    [key: string]: any;
+}
+
+export class JsonResponseUtil {
+    public success(res: Response, data: JsonResponseData): void {
+        data = Object.assign(data, { status: httpStatus.OK });
+        res.status(httpStatus.OK).json(data);
+    }
+
+    public error(res: Response, error: unknown): void {
+        let errorData: JsonResponseData;
+
+        if (error instanceof HttpClientError) errorData = error.toJSON();
+        else errorData = HttpClientError.internalServerError().toJSON();
+
+        res.status(errorData.status).json(errorData);
+    }
+
+    public badRequest(res: Response, message: string): void {
+        const badRequestError = HttpClientError.badRequest(message);
+        this.error(res, badRequestError);
+    }
+
+    public notFound(res: Response): void {
+        const notFoundError = HttpClientError.notFound();
+        this.error(res, notFoundError);
+    }
+
+    public internalServerError(res: Response): void {
+        const internalServerError = HttpClientError.internalServerError();
+        this.error(res, internalServerError);
+    }
+}
