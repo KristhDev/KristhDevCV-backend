bba9e7763ef65d27c26c9dff9a1faee590e4f092
KristhDev <kristhdev@gmail.com>
2025-08-31 19:04:24 -0600

    feat(core): initialize and start server
    The index.ts file acts as the main entry point for the application. It sets up module aliases, imports path configurations, initializes the server with a logger, and starts listening for incoming requests. This establishes the basic structure for the application's startup process.

---
+++ index.ts
@@ -0,0 +1,9 @@
+import 'module-alias/register';
+import './paths';
+
+import { loggerAdapter } from '@config/di';
+
+import { Server } from '@server';
+
+const server = new Server(loggerAdapter);
+server.listen();
