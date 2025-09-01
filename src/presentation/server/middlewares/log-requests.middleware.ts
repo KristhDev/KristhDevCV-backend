06627e5178de13fc4650d75fb79ecfd85639b3e5
KristhDev <kristhdev@gmail.com>
2025-09-01 13:17:38 -0600

    refactor(middlewares): enhance log error context
    Refines how error contexts are captured and logged by separating response and content details. This ensures more comprehensive information is available for debugging when an error occurs.
    
    Also, adds JSDoc comments to key methods for improved code documentation and organizes imports for better readability.

--- src/presentation/server/middlewares/log-requests.middleware.ts
+++ src/presentation/server/middlewares/log-requests.middleware.ts
@@ -1,9 +1,12 @@
 import { NextFunction, Request, Response, Send } from 'express';
 
+/* Contracts */
 import { LoggerAdapterContract, UserAgentAdapterContract } from '@domain/contracts/adapters';
 
+/* Interfaces */
 import { UserAgentParsed } from '@infrastructure/interfaces';
 
+/* Middlewares */
 import { BaseMiddleware } from './base.middleware';
 
 export class LogRequestsMiddleware extends BaseMiddleware {
@@ -14,19 +17,37 @@ export class LogRequestsMiddleware extends BaseMiddleware {
         super();
     }
 
+    /**
+     * Finishes the request.
+     *
+     * @param {string} logMessage - The log message.
+     * @param {UserAgentParsed} userAgent - The user agent.
+     * @param {Request} req - The request object.
+     * @param {Response} res - The response object.
+     * @return {void} This function does not return a value.
+     */
     private onFinish(logMessage: string, userAgent: UserAgentParsed, req: Request, res: Response): void {
         const content = (res as any).content;
+        const response = (res as any).response;
 
-        const context = {
-            ...userAgent,
-            message: content?.message || undefined,
-            status: content?.status || res.statusCode,
-        }
+        const respContext = { ...userAgent, ...response }
+        const context = { ...userAgent, ...content }
 
         if (context.status >= this.httpStatus.OK && context.status < 300) this.loggerAdapter.success(logMessage, context);
-        else this.loggerAdapter.error(logMessage, context);
+        else {
+            this.loggerAdapter.error(logMessage, respContext);
+            this.loggerAdapter.error(logMessage, context);
+        }
     }
 
+    /** 
+     * Sends the response.
+     *
+     * @param {any} body - The response body.
+     * @param {Response} res - The response object.
+     * @param {Send} send - The send function.
+     * @return {Response} The response object.
+     */
     private onSendResponse(body: any, res: Response, send: Send): Response {
         let parsedBody = JSON.parse(JSON.stringify(body));
 
@@ -40,6 +61,14 @@ export class LogRequestsMiddleware extends BaseMiddleware {
         return send.call(res, body);
     }
 
+    /**
+     * Handles the request.
+     *
+     * @param {Request} req - The request object.
+     * @param {Response} res - The response object.
+     * @param {NextFunction} next - The next function.
+     * @return {void} This function does not return a value.
+     */
     public handle(req: Request, res: Response, next: NextFunction): void {
         const logMessage = `${ req.method } ${ req.path }`;
         const userAgentHeader = req.headers['user-agent'];
