15f878e4e5e5d0b2ebda9d76be6d9783c09f48a6
KristhDev <kristhdev@gmail.com>
2025-09-01 12:33:36 -0600

    feat(di): inject time adapter into server
    Injects the time adapter dependency into the server constructor to manage time-related operations, improving modularity and testability.

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import 'module-alias/register';
 import './paths';
 
-import { loggerAdapter, userAgentAdapter } from '@config/di';
+import { loggerAdapter, timeAdapter, userAgentAdapter } from '@config/di';
 
 import { Server } from '@server';
 
-const server = new Server(loggerAdapter, userAgentAdapter);
+const server = new Server(loggerAdapter, timeAdapter, userAgentAdapter);
 server.listen();
