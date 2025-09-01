236cd4cfa4eacc0c852e812abad667ee19247d11
KristhDev <kristhdev@gmail.com>
2025-09-01 12:35:10 -0600

    feat(server): add http status constants to base controller
    Provide direct access to HTTP status codes for all inheriting controllers, centralizing common utilities. Ensure the jsonResponse utility's immutability by marking it as readonly.

--- src/presentation/server/controller/base.controller.ts
+++ src/presentation/server/controller/base.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 
+import { httpStatus } from '@application/constants';
+
 import { JsonResponseUtil } from '@server/utils';
 
 export abstract class BaseController {
-    protected jsonResponse: JsonResponseUtil;
+    protected readonly jsonResponse: JsonResponseUtil;
+    protected readonly httpStatus = httpStatus;
 
     public constructor (
     ) {
