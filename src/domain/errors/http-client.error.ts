af965881edd3efa42695362679fab6a10e21e7a0
KristhDev <kristhdev@gmail.com>
2025-08-31 20:14:09 -0600

    feat(errors): introduce domain error handling
    Introduces a foundational error handling system within the domain layer. Provides `BaseError` for common error structure and `HttpClientError` for HTTP-specific errors. Includes utility methods for common HTTP status codes, ensuring consistent error management and serialization.

---
+++ src/domain/errors/http-client.error.ts
@@ -0,0 +1,52 @@
+import { httpStatus } from '@application/constants';
+
+import { BaseError } from './base.error';
+
+export interface HttpClientErrorJson {
+    message: string;
+    status: number;
+}
+
+export class HttpClientError extends BaseError<HttpClientErrorJson> {
+    constructor(message: string, public status: number) {
+        super(message);
+        this.name = 'HttpClientError';
+    }
+
+    public toJSON(): HttpClientErrorJson {
+        return {
+            message: this.message,
+            status: this.status
+        }
+    }
+
+    /**
+     * Returns an HttpClientError with a status code of 400 (Bad Request).
+     *
+     * @param {string} message The error message.
+     * @return {HttpClientError} An instance of HttpClientError with a status code of 400.
+     */
+    public static badRequest(message: string): HttpClientError {
+        return new HttpClientError(message, httpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Returns an HttpClientError with a status code of 404 (Not Found).
+     *
+     * @param {string} [message] The error message. If not provided, a default message is used.
+     * @return {HttpClientError} An instance of HttpClientError with a status code of 404.
+     */
+    public static notFound(message?: string): HttpClientError {
+        return new HttpClientError(message || "Sorry, but we couldn't find the requested route.", httpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Returns an HttpClientError with a status code of 500 (Internal Server Error).
+     *
+     * @param {string} [message] The error message. If not provided, a default message is used.
+     * @return {HttpClientError} An instance of HttpClientError with a status code of 500.
+     */
+    public static internalServerError(message?: string): HttpClientError {
+        return new HttpClientError(message || 'An unexpected error occurred. Please try again later.', httpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
