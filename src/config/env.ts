22470bbc9b7491c8329fc7c43c9d5e4e97bd0579
KristhDev <kristhdev@gmail.com>
2025-08-31 23:42:56 -0600

    feat(env): introduce app_env for environment identification
    Adds the APP_ENV environment variable to enable the application to differentiate between various deployment environments, such as development, staging, or production. This allows for more flexible and robust environment-specific configurations and behaviors.

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
+    APP_ENV: process.env.APP_ENV!,
     LOGTAIL_TOKEN: process.env.LOGTAIL_TOKEN!,
-    PORTFOLIO_API_URL: process.env.PORTFOLIO_API_URL!,  
     PORT: process.env.PORT!,
+    PORTFOLIO_API_URL: process.env.PORTFOLIO_API_URL!,  
 }
