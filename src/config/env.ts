b2da6fd3e3648fd9f2d02c732ce3aecbee6ba802
KristhDev <kristhdev@gmail.com>
2025-09-08 23:06:44 -0600

    refactor(env): rename env vars and add logtail source url
    Renames environment variables for improved clarity and consistency. Adds a new variable to configure the logtail source URL, enhancing logging flexibility.

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,8 @@ dotenv.config();
 
 export const env = {
     APP_ENV: process.env.APP_ENV!,
-    LOGTAIL_TOKEN: process.env.LOGTAIL_TOKEN!,
-    PORT: process.env.PORT!,
+    APP_PORT: process.env.APP_PORT!,
+    LOGTAIL_SOURCE_TOKEN: process.env.LOGTAIL_SOURCE_TOKEN!,
+    LOGTAIL_SOURCE_URL: process.env.LOGTAIL_SOURCE_URL!,
     PORTFOLIO_API_URL: process.env.PORTFOLIO_API_URL!,  
 }
