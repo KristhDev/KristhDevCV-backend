72296e085988e7ff44f5203a2a7abad448d984ea
KristhDev <kristhdev@gmail.com>
2025-10-02 22:07:29 -0600

    feat(config): add app token environment variable
    Makes the APP_TOKEN available in the application's environment configuration for use in various services.

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ dotenv.config();
 export const env = {
     APP_ENV: process.env.APP_ENV!,
     APP_PORT: process.env.APP_PORT!,
+    APP_TOKEN: process.env.APP_TOKEN!,
     LOGTAIL_SOURCE_TOKEN: process.env.LOGTAIL_SOURCE_TOKEN!,
     LOGTAIL_SOURCE_URL: process.env.LOGTAIL_SOURCE_URL!,
     PORTFOLIO_API_URL: process.env.PORTFOLIO_API_URL!,  
